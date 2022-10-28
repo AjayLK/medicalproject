47375b9d11d9f8f3ae87086ed689aaddaeede356
Ajay <ajaykshirsagar500@gmail.com>
2022-10-28 23:05:11 +0530

    project configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+CSV_FILE_PATH = "enter path"
+
+PORT_NUMBER = 9689
+
+PICKLE_FILE_PATH = os.path.join("medical_insurance", "medical_insurance_model.pkl")
+
+JSON_FILE_PATH = os.path.join("medical_insurance", "medical_insurance_encoding_col.json")
+
